27799e76d010e9ff588cd73c03fa1ccb91cf5ada
AnutKu <annyshka.k1608@gmail.com>
2024-04-19 01:23:12 +0300

    created ReactorHolder class and made some improvments with importing

---
+++ src/main/java/ReactorHolder.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReactorHolder {
+    private Map<String, Reactor> reactorMap;
+
+    public ReactorHolder() {
+        this.reactorMap = new HashMap<>();
+    }
+    public void addReactor(String key, Reactor reactor) {
+        reactorMap.put(key, reactor);
+    }
+    public Map<String, Reactor> getReactorMap() {
+        return reactorMap;
+    }
+}
